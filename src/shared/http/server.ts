eadc5c30b9a3c62a4a76a365edf0efa3a0d9ab0b
Willian Giovanni <willianclemente10@gmail.com>
2023-08-03 14:59:54 -0300

    feat: validation of request data

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import '@shared/typeorm';
+import { errors } from 'celebrate';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
@@ -12,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof AppError) {
